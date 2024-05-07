8351c70b3b270265e5bbf57e4abc8bf2c1b4f9b8
axel669 <haryuu.hanekata@gmail.com>
2024-05-06 22:55:39 -0700

    [update] move some props around for windstorm changes

--- src/info/titlebar.svelte
+++ src/info/titlebar.svelte
@@ -4,22 +4,28 @@
     export let color
     export let fill = false
 
-    $: wind = {
+    $: titleProps = {
         "$color": color,
         "$fill": fill,
+        $title: true,
+        grid: true,
+        p: "0px"
+    }
+    $: wind = {
         ...$$restProps
     }
 </script>
 
 <ws-titlebar use:wsx={wind}>
+    {#if $$slots.title}
+        <div use:wsx={titleProps}>
+            <slot name="title" />
+        </div>
+    {/if}
+
     {#if $$slots.menu}
         <div ws-x="[$menu] [grid] [p 0px]">
             <slot name="menu" />
-        </div>
-    {/if}
-    {#if $$slots.title}
-        <div ws-x="[$title] [grid] [p 0px]">
-            <slot name="title" />
         </div>
     {/if}
     {#if $$slots.action}
