8755c8c2a27f95a6e08ea2850abc95589b9e90bd
axel669 <haryuu.hanekata@gmail.com>
2024-06-10 01:07:46 -0700

    [update] radio inputs for windstorm 0.4.x, rename to group, add examples

---
+++ docsite/example/radio-group.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { RadioGroup, Grid, Titlebar, Text } from "@axel669/zephyr"
+
+    const options = [
+        { label: "Number", value: 1 },
+        { label: "Array", value: [], $color: "@secondary" },
+        { label: "String", value: "3", "$color": "@warning" },
+        { label: "Really any value", value: new Date() },
+    ]
+    let value = null
+</script>
+
+<pre>Value: {JSON.stringify(value)}</pre>
+
+<Titlebar>
+    <Text title slot="title">
+        Flex Layout (default)
+    </Text>
+</Titlebar>
+<RadioGroup {options} bind:value />
+
+<Titlebar>
+    <Text title slot="title">
+        Grid Layout Example
+    </Text>
+</Titlebar>
+<RadioGroup {options} bind:value layout={Grid} gr.cols="1fr 1fr" />
