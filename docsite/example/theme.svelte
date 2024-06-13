3aef9bb433d759ea996f21e3ca25c915f111baca
axel669 <haryuu.hanekata@gmail.com>
2024-06-12 18:04:39 -0700

    [add] basic theme example

---
+++ docsite/example/theme.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { Select, Paper, Button, wsx } from "@axel669/zephyr"
+
+    let theme = "dark"
+
+    const options = [
+        { label: "Light", value: "light" },
+        { label: "Dark", value: "dark" },
+        { label: "Tron", value: "tron" },
+    ]
+</script>
+
+<Select {options} bind:value={theme} label="Theme" color="@primary" />
+<div use:wsx={{ "@@theme": theme }}>
+    <Paper card>
+        <div>Some text</div>
+        <Button color="@primary" outline>Primary Button</Button>
+        <Button color="@secondary" fill>Secondary Button</Button>
+    </Paper>
+</div>
